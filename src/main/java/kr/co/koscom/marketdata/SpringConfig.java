6cb4acf95164360ab68f8bc7681d93b0d0d1662b
syhoons2 <syhoons2@naver.com>
2018-02-02 08:19:56 +0900

    reset

---
+++ src/main/java/kr/co/koscom/marketdata/SpringConfig.java
@@ -0,0 +1,18 @@
+package kr.co.koscom.marketdata;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class SpringConfig extends WebMvcConfigurerAdapter {
+	@Bean
+	public ViewResolver getViewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/jsp/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+}
